38f72b0b719858d8501dff0b476531b909087da3
Noah Fairbairn <noahfairbairn@gmail.com>
2019-02-01 03:58:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,9 @@ def my_collect (array)
   i = 0
   while i < array.length
     yield (array[i])
+    result = []
+    array.each do |element|
+      result << block.call(element)
     i = i + 1
   end
   array
